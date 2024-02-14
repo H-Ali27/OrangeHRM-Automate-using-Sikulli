1daa4ce8b6c385715bef77ab096f0aa89b20d643
Hamza Ali <hamzaali528@gmail.com>
2024-02-14 23:02:16 +0500

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SikuliProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SikuliProject")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ab2c9188-933a-4070-83c0-7ef636e3cb14")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using SikuliModule;
+using System;
+using WindowsInput;
+
+namespace SikuliProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ExecutionClass()
+        {
+            IWebDriver driver = new ChromeDriver();
+            InputSimulator sim = new InputSimulator();
+            driver.Manage().Window.Maximize();
+
+            driver.Url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+
+            SikuliAction.Click(@"E:\Seleium\SikuliProject\Pictures\005.png");
+            sim.Keyboard.TextEntry("Admin");
+
+            SikuliAction.Click(@"E:\Seleium\SikuliProject\Pictures\004.png");
+            sim.Keyboard.TextEntry("admin123");
+
+            SikuliAction.Click(@"E:\Seleium\SikuliProject\Pictures\003.png");
+
+            SikuliAction.Click(@"E:\Seleium\SikuliProject\Pictures\002.png");
+
+            SikuliAction.Click(@"E:\Seleium\SikuliProject\Pictures\001.png");
+        }
+    }
+}
